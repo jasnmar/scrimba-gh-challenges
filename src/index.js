166e87ce291c43fe71aace06c3c059d589c41abb
jasnmar <jasnmar@gmail.com>
2024-06-04 13:03:42 -0400

    Challenge 3

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-/* Whispering function 
-Write a function `whisper` that takes in a sentence 
-and returns a new sentence in all lowercase letters with
-"shh..." at the beginning. 
+/* Alternating Caps 
+ Write a function that takes in a string of letters
+ and returns a sentence in which every other letter is capitalized.
 
-The function should also remove an exclamation point
-at the end of the sentence, if there is one. 
-
-Example 
-input: "The KITTENS are SLEEPING!"
-output: "shh... the kittens are sleeping"
-
-Hint: endsWith and slice
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
 */
 
-console.log(whisper("PLEASE STOP SHOUTING."));
-console.log(whisper("MA'AM, this is a Wendy's!"));
-
-function whisper(str) {
-  str = str.endsWith("!") ? str.slice(0, str.length-1) : str
-  console.log('str: ', str)
-  return "shh... " + str
-    .toLowerCase()
-    
+function altCaps(str){
+  const strArray = str.split("")
+  for(let ii=0;ii<strArray.length;ii++){
+    if(!(ii % 2)) {
+      strArray[ii] = strArray[ii].toUpperCase()
+    } else {
+      strArray[ii] = strArray[ii].toLowerCase()
+    }
+  }
+  const newStr = strArray.join("")
+  return newStr;
 }
+
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
