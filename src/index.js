d8f522b83fd829f05c9feb781cbbac24769e5a01
jasnmar <jasnmar@gmail.com>
2024-06-04 15:18:51 -0400

    Challenge 11

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-/*  
-Grandpa's hand isn't as steady as it used to be. You finally convinced him
-to start using a password manager, but he accidentally typed and saved his
-password with a bunch of extra characters. Help him recover his password by 
-removing all the duplicate characters. 
+/* 
+How often do the letters in your name repeat? 
 
-Your function should take in a string of characters and return a
-string with the duplicate characters removed. Assume that your input
-is lowercased with only letters and numbers.  
+Write a function that counts how many times each letter of your name
+occurs. Your function should take in your first and last name and return
+an object where the keys are each character in your name, and the value
+is how many times that character appears in your name. 
 
-Example input: "aabbccb1212"
-Example output: "abc12"
+Example input: "Peggy Porth"
+Example output: {p: 2, e: 1, g: 2, y: 1, o: 1, r: 1, t: 1, h: 1}
+
+Your function should NOT count spaces and should not be case sensitive (a
+lowercase t and a capital T should be considered the same character).
+
 */ 
-const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23"; 
- 
-function removeDupeChars(chars){
-  const charsArray = chars.split("")
-  const uniqueChars = []
-  charsArray.forEach(char => {
-    if(!(uniqueChars.find((value) => value===char))) {
-      uniqueChars.push(char)
+
+function countChars(str){
+  str = str.toLowerCase()
+  const array = str.split("")
+  const myObject = {}
+  array.forEach(element => {
+    if(!(element===" ")) {
+      if(!(myObject[element])) {
+        myObject[element] = 1
+      } else {
+        myObject[element] += 1
+      }
     }
-    
-  });
-  return uniqueChars.join("")
-    
+    });
+  return myObject
 }
 
-console.log(removeDupeChars("aabbccb1212") )
-console.log(removeDupeChars(password));
+console.log(countChars("Peggy Porth"));
