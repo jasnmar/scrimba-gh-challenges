793ce02061ce08e83b2d94406419af420b5b0b06
jasnmar <jasnmar@gmail.com>
2024-06-04 14:56:37 -0400

    Challenge 9

--- src/index.js
+++ src/index.js
@@ -1,45 +1,33 @@
-/* We Come in Peace!  
-We've received what (we assume) is a message of peace and brotherhood from 
-an alien planet. They almost got it right, but the messages are 
-backward. Write functions to reverse the backward messages so we can 
-read what they have to say! 
-*/ 
+/* 
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
 
-const title = ":htraE no od ot ffutS";
-const messages = [
-            "maerc eci yrT",
-            "rewoT leffiE tisiV",
-            "noom eht ot snamuh etacoleR",
-            "egrahc ni stac tuP", 
-        ]
+Example input: "motorbike"
+Example output: false
 
-/* Step 1: Reverse a string
-Write a function that takes in a string and returns the reverse 
-of that string. An interviewer may want to check if you know your
-string methods, or may want to know if you can reverse a string manually. 
-Practice both ways! 
+Example input: "rotator" 
+Example output: true
+*/
 
-Example input: !htrae ot emocleW
-Example output: Welcome to earth!
-*/  
-
-function reverseString(arr){
+function isPalindrome(str){
+  const arr = str.split("")
+  for (ii=0; ii < arr.length; ii++) {
+    // console.log('arr[ii]: ', arr[ii])
+    // console.log('arr[arr.length-ii]: ', arr[arr.length-1-ii])
+    if(!(arr[ii]===arr[arr.length-1-ii])) {
+      return false
+    }
+  }
+  return true
     
-    return arr.split("").reverse().join("");
 }
 
-/*
-Step 2: Now we'll reverse all strings in an array. Write a function that takes in
-an array of strings and returns a new array with all strings reversed.
-
-You can use reuse your reverseString() function, use string methods, or 
-reverse the strings manually. 
-*/ 
-
-function reverseStringsInArray(arr){
-    newArray = arr.map((string) => reverseString(string))
-    return newArray;
-}
-
-console.log(reverseString(title));
-console.log(reverseStringsInArray(messages));
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
