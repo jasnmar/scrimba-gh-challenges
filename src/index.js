a1e3157f684aa798bece829d7b6967b52493556f
jasnmar <jasnmar@gmail.com>
2024-06-04 15:43:10 -0400

    Challenge 13a

--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
-/* Chef Mario's Recipe Book 
-Chef Mario was in the middle of writing his cookbook masterpiece
-when he spilled coffee on his keyboard! Now all his recipes have repeat
-ingredients.
 
-Help save Chef Mario's cookbook by writing a function that takes in an array 
-and returns a new array with all the duplicates removed. 
+/* 
+Scrimba mascot Pumpkin has won the grand prize at an international 
+cat show. Below are Pumpkin's scores from the judges, as well as all the 
+prizes he's won. In all the excitement of victory,
+they've become a jumbled mess of nested arrays. Let's 
+help Pumpkin by sorting it out. 
 
-Example input: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops", "🦄 unicorn", "🍭 lollipops"];
-Example output: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops"];
-*/ 
+Write a function to flatten nested arrays of strings or
+numbers into a single array. There's a method
+for this, but pratice both doing it manually and using the method. 
 
-const eggScrambleRecipe = [
-  "🥓 bacon",
-  "🥓 bacon", 
-  "🍳 eggs",
-  "🫑 green peppers",
-  "🧀 cheese",
-  "🌶️ hot sauce",
-  "🥓 bacon",
-  "🥦 broccoli", 
-  "🧀 cheese",
-  "🥦 broccoli", 
-  "🌶️ hot sauce"
-]
+Example input: [1, [4,5], [4,7,6,4], 3, 5]
+Example output: [1, 4, 5, 4, 7, 6, 4, 3, 5]
+*/
 
-function removeDupesFromArray(arr){
-  const result = []
-  arr.forEach(element => {
-    if(!(result.find((item) => element === item))) {
-      result.push(element)
-    }
-  });
-  return result
+const input = [1, [4,5], [4,7,6,4], 3, 5]
+
+const kittyScores = [
+  [39, 99, 76], 89, 98, [87, 56, 90], 
+  [96, 95], 40, 78, 50, [63]
+];
+
+const kittyPrizes = [
+  ["💰", "🐟", "🐟"], "🏆", "💐", "💵", ["💵", "🏆"],
+  ["🐟","💐", "💐"], "💵", "💵", ["🐟"], "🐟"
+];
+
+function flatten(arr){
+  return arr.flat()
 }
 
-console.log(removeDupesFromArray(eggScrambleRecipe));
+console.log(flatten(kittyPrizes));
+console.log(flatten(kittyScores));
+console.log(flatten(input));
