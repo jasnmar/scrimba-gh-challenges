62d1a889914e0b732c9203ec1294654aa86a274a
jasnmar <jasnmar@gmail.com>
2024-06-04 15:06:44 -0400

    Challenge 10

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-/* 
-Palindromes are words that are the same forward or backward. For example, 
-the words "noon" and "kayak" are a palindromes.
+/*  
+Grandpa's hand isn't as steady as it used to be. You finally convinced him
+to start using a password manager, but he accidentally typed and saved his
+password with a bunch of extra characters. Help him recover his password by 
+removing all the duplicate characters. 
+
+Your function should take in a string of characters and return a
+string with the duplicate characters removed. Assume that your input
+is lowercased with only letters and numbers.  
+
+Example input: "aabbccb1212"
+Example output: "abc12"
+*/ 
+const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23"; 
  
-Write a function to check if a lowercased string of letters is a palindrome. 
-If the word is palindrome, return true. If it isn't, return false.
-
-Example input: "motorbike"
-Example output: false
-
-Example input: "rotator" 
-Example output: true
-*/
-
-function isPalindrome(str){
-  const arr = str.split("")
-  for (ii=0; ii < arr.length; ii++) {
-    // console.log('arr[ii]: ', arr[ii])
-    // console.log('arr[arr.length-ii]: ', arr[arr.length-1-ii])
-    if(!(arr[ii]===arr[arr.length-1-ii])) {
-      return false
+function removeDupeChars(chars){
+  const charsArray = chars.split("")
+  const uniqueChars = []
+  charsArray.forEach(char => {
+    if(!(uniqueChars.find((value) => value===char))) {
+      uniqueChars.push(char)
     }
-  }
-  return true
+    
+  });
+  return uniqueChars.join("")
     
 }
 
-// Test your function
-console.log(isPalindrome("abba"));
-console.log(isPalindrome("civic"));
-console.log(isPalindrome("octopus"));
-console.log(isPalindrome("pumpkins"));
-console.log(isPalindrome("madam"));
+console.log(removeDupeChars("aabbccb1212") )
+console.log(removeDupeChars(password));
