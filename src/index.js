72b6e408e02ff7cbcefbc22edc2ba8698fb4e9e3
jasnmar <jasnmar@gmail.com>
2024-06-04 12:51:57 -0400

    Challenge 2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
-/* Panic function 
-Write a PANIC! function. The function should take in a sentence and return the same
-sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
-built in string methods. 
+/* Whispering function 
+Write a function `whisper` that takes in a sentence 
+and returns a new sentence in all lowercase letters with
+"shh..." at the beginning. 
 
-If the string is a phrase or sentence, add a 😱 emoji in between each word. 
+The function should also remove an exclamation point
+at the end of the sentence, if there is one. 
 
-Example input: "Hello"
-Example output: "HELLO!"
+Example 
+input: "The KITTENS are SLEEPING!"
+output: "shh... the kittens are sleeping"
 
-Example input: "I'm almost out of coffee"
-Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+Hint: endsWith and slice
 */
 
+console.log(whisper("PLEASE STOP SHOUTING."));
+console.log(whisper("MA'AM, this is a Wendy's!"));
 
-// Test your function
-console.log(panic("I'm almost out of coffee")); 
-console.log(panic("winter is coming"))
-
-function panic(str) {
-  const words = str.split(" ")
-  const nWords = words.map((word) => {
-    const nWord = word.toUpperCase()
-    return nWord + " 😱 "
-  })
-  let finalStr = nWords.join("")
-  const strLength = finalStr.length
-  const lastStr = finalStr.slice(0, finalStr.length - 4)
-  return lastStr+"!"
+function whisper(str) {
+  str = str.endsWith("!") ? str.slice(0, str.length-1) : str
+  console.log('str: ', str)
+  return "shh... " + str
+    .toLowerCase()
+    
 }
