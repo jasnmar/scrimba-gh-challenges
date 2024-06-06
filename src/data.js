859a465028297a7ab62bf58ddb3c6ac7cc992a7a
jasnmar <jasnmar@gmail.com>
2024-06-06 14:07:06 -0400

    Challenge 23

--- src/data.js
+++ src/data.js
@@ -1,38 +1,59 @@
 export default [
   {
       id: 1,
-      title: "Stranger Scrims", 
-      duration: 40, 
-      tags: ["supernatural", "horror", "drama"], 
+      title: "Scrimba Podcast", 
+      duration: 50, 
+      tags: ["education", "jobs", "technology"], 
+      hosts: ["Alex Booker"], 
+      rating: 10,
+      genre: "education",
+      paid: false
   },
   {
       id: 2,
-      title: "The Scrim of the Dragon", 
-      duration: 60, 
-      tags: ["drama", "fantasy"], 
+      title: "Crime Fan", 
+      duration: 150, 
+      tags: ["crime", "entertainment", "mature"], 
+      hosts: ["Bob Smith", "Camilla Lambert"], 
+      genre: "true crime",
+      rating: 5, 
+      paid: true
   },
   {
       id: 3,
-      title: "Scrim Hunters", 
-      duration: 22, 
-      tags: ["reality", "home improvement"], 
+      title: "Mythical Creatures", 
+      duration: 99, 
+      tags: ["entertainment", "general", "unicorns"], 
+      hosts: ["Esmerelda Shelley", "Duke Dukington", "Felix the Cat"], 
+      genre: "fantasy",
+      rating: 8,
+      paid: true
   },
   {
-      id: 4,
-      title: "This Old Scrim", 
-      duration: 30, 
-      tags: ["reality", "home improvement"], 
+      title: "Crime Crime Crime", 
+      duration: 70, 
+      tags: ["crime", "entertainment", "mature"], 
+      hosts: ["Jessica Jones", "Humphrey Bogart", "Inspector Gadget"], 
+      genre: "true crime",
+      rating: 6, 
+      paid: true
   },
   {
-      id: 5,
-      title: "What We Do in the Scrim", 
+      title: "Something about Witches", 
+      duration: 35, 
+      tags: ["fantasy", "entertainment"], 
+      hosts: ["Frewin Wyrm", "Evanora Highmore"], 
+      genre: "fantasy",
+      rating: 8,
+      paid: false
+  },
+  {
+      title: "Coding Corner", 
       duration: 55, 
-      tags: ["drama", "comedy", "supernatural"], 
-  },
-  {
-      id: 6,
-      title: "The Scrimdalorian", 
-      duration: 58, 
-      tags: ["fantasy", "sci-fi", "adventure"], 
+      tags: ["education", "jobs", "technology"], 
+      hosts: ["Treasure Porth", "Guil Hernandez", "Tom Chant"], 
+      genre: "education",
+      rating: 9,
+      paid: false
   },
 ]
