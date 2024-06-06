0f72762463a26667ed64cbc14ccbaa9236956175
jasnmar <jasnmar@gmail.com>
2024-06-06 13:52:08 -0400

    Challenge 22

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import products from "./data.js";
+import mediaData from "./data.js";
 
-/*
-    You're online shopping for holiday gifts, but money is tight
-    so we need to look at the cheapest items first. 
-    Use the built in sort() method to write a function that returns a new array of
-    products sorted by price, cheapest to most expensive. 
-    
-    Then log the item and the price to the console: 
-    
-    💕,0
-    🍬,0.89
-    🍫,0.99
-    🧁,0.99
-    📚,0.99
-    ... continued
-*/
+/* Find All Unique Tags 
 
-function sortProducts(data){
-  return data.sort(compare)
-  function compare(a,b) {
-    return a.price - b.price
-  }
+As a software dev at ScrimFlix, you're working on a feature 
+to let users browse TV shows by tag. The first step is to collect all 
+the tags from our data into a new array. Then we'll need 
+to filter out the duplicate tags. 
+
+Write a function that takes in the media data and returns
+a flat array of unique tags.
+
+Expected output: 
+["supernatural", "horror", "drama",
+"fantasy", "reality", "home improvement", "comedy", "sci-fi", "adventure"]
+
+*/ 
+
+function getUniqueTags(data){
+  const result = []
+  const arr = data.map((show)=> {
+    result.push(show.tags)
+    return show.tags
+  })
+  return [...new Set(result.flat())]
 }
 
-const listByCheapest = sortProducts(products);
-console.log(listByCheapest);
-
+console.log(getUniqueTags(mediaData));
 
